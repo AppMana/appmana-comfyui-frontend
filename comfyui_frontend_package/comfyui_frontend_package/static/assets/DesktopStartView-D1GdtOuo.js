6283a719a8a516003ea17441f60c3e3e9395c670
doctorpangloss <@hiddenswitch.com>
2025-07-11 13:00:00 -0700

    compile

---
+++ comfyui_frontend_package/comfyui_frontend_package/static/assets/DesktopStartView-D1GdtOuo.js
@@ -0,0 +1,22 @@
+import { defineComponent, openBlock, createBlock, withCtx, createVNode, unref } from "./vendor-vue-Dj1-cm8e.js";
+import { script$30 as script } from "./vendor-primevue--_uihavt.js";
+import { _sfc_main as _sfc_main$1 } from "./BaseViewTemplate-DFm7rk9i.js";
+import "./index-yT-4ROaT.js";
+import "./vendor-vue-i18n-CepkKHgT.js";
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  __name: "DesktopStartView",
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createBlock(_sfc_main$1, { dark: "" }, {
+        default: withCtx(() => [
+          createVNode(unref(script), { class: "m-8 w-48 h-48" })
+        ]),
+        _: 1
+      });
+    };
+  }
+});
+export {
+  _sfc_main as default
+};
+//# sourceMappingURL=DesktopStartView-D1GdtOuo.js.map
